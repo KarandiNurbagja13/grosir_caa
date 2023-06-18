0bb3b94d49fc1fb188f7d5012fa9267c8e949094
KarandiNurbagja13 <68455191+KarandiNurbagja13@users.noreply.github.com>
2023-06-19 00:42:00 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Toggle class active untuk hamburger menu
-const navbarNav = document.querySelector(".navbar-nav");
+const navbarNav = document.querySelector(".navbar-nav active");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
